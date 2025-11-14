8749dccce207f7317ca1860142a34cd3c350499e
Tim Klimpel <tim.klimpel@gmail.com>
2025-11-14 00:04:06 -0500

    migrated and cleaned directory structure

--- servers/gateway-server/src/main/java/com/tkforgeworks/cookconnect/gatewayserver/GatewayServerApplication.java
+++ servers/gateway-server/src/main/java/com/tkforgeworks/cookconnect/gatewayserver/GatewayServerApplication.java
@@ -1,4 +1,4 @@
-package com.tkforgeworks.gatewayserver;
+package com.tkforgeworks.cookconnect.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
